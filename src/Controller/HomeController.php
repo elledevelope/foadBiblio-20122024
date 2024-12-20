ba51529f0cc9ef7c975b335fe25e0b7ad4755543
elmira <elledevelope@gmail.com>
2024-12-20 14:47:38 +0100

    Add LivreRepository and CategoryRepository in HomeController to display data from db in cards on the main page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(LivreRepository $livreRepository, CategoryRepository $categoryRepository): Response
     {
-        return $this->render('home/index.html.twig');
+ 
+        $livres = $livreRepository->findAll();
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'livres' => $livres,
+            'categories' => $categories, 
+        ]);
     }
 }
